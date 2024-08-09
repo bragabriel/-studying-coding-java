b1e12df24725a36c6395034ac860b25e333ce88b
Bragabriel <gabriel.bragasilva@hotmail.com>
2024-08-09 15:29:17 -0300

    add files

---
+++ Function/FunctionL.java
@@ -0,0 +1,18 @@
+package Function;
+
+import java.util.function.Function;
+
+public class FunctionL {
+    public static void main(String[] args) {
+        Function<Integer, Double> calculo = a -> a * 2d;
+
+        //Quando chamado o apply, vai primeiro executar o APPLY, e depois o AndTHEN
+        calculo = calculo.andThen(a -> a + 10);
+
+        //Quando chamado o compose, vai executar primeiro o COMPOSE, e depois o APPLY
+        calculo = calculo.compose(a -> a + 10);
+
+        Double resultado = calculo.apply(10);
+        System.out.println(resultado);
+    }
+}
